b0ebfdcf1161e67656360c99a5a4f11d71d9a098
minhduc862005 <duchm9479@ut.edu.vn>
2025-08-26 22:41:29 +0700

    khởi tạo

--- hello.py/hello.py
+++ hello.py/hello.py
@@ -1,2 +1,27 @@
-print("Hello GitHub!")
-print("xinchao")
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace gameponggk
+{
+    public partial class Form1 : Form
+    {
+        // tốc độ di chuyển
+        int playerSpeed = 8;
+
+        // tốc độ bóng
+        int ballXSpeed = 5;
+        int ballYSpeed = 5;
+
+        // điểm số
+        int player1Score = 0;
+        int player2Score = 0;
+
+        // trạng thái phím
+        bool wDown, sDown, upDown, downDown;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ResetBall();
+        }
